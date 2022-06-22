a02ae300402e17e89df18351ed24f6d34eacf03b
Md Miskatur Rahman <59787966+sajjatflutterdeveloper@users.noreply.github.com>
2022-06-22 00:48:25 -0700

    IOS Work

--- android/app/src/main/kotlin/com/example/chatting/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.example.chatting
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
--- android/app/src/main/kotlin/com/yshof/chatter/MainActivity.kt
+++
@@ -1,6 +0,0 @@
-package com.yshof.chatter
-
-import io.flutter.embedding.android.FlutterActivity
-
-class MainActivity: FlutterActivity() {
-}
